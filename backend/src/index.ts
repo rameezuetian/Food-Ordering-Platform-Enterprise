b1936bb853e433daa4ba407e2efc581003286c72
rameezuetian <muhammadrameez262@gmail.com>
2026-09-16 11:09:22 +0500

    Backend Setup

---
+++ backend/src/index.ts
@@ -0,0 +1,42 @@
+import express, { type Request, type Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import dns from "dns";
+
+// Use Google DNS for Node.js DNS resolution
+dns.setServers([
+    "8.8.8.8",
+    "8.8.4.4",
+]);
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+    res.json({ message: "Hello" });
+});
+
+const PORT = 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+if (!MONGO_URI) {
+    throw new Error("MONGO_URI is not defined in .env");
+}
+
+mongoose
+    .connect(MONGO_URI)
+    .then(() => {
+        console.log("Connected to database");
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on localhost:${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("MongoDB connection failed:");
+        console.error(error);
+        process.exit(1);
+    });
