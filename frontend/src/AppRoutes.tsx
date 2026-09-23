fdee96b65c9e9d2881013c14aa88758f7c1425ec
rameezuetian <muhammadrameez262@gmail.com>
2026-09-23 12:23:42 +0500

    Protected Route

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,21 +1,35 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import Layout from "./layouts/Layout"
-import HomePage from "./pages/HomePage"
-import AuthCallbackPage from "./pages/AuthCallbackPage"
-import UserProfilePage from "./pages/UserProfilePage"
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/Layout";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
 
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout showHero>
+            <HomePage />
+          </Layout>
+        }
+      />
+      <Route path="/auth-callback" element={<AuthCallbackPage />} />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={
+            <Layout>
+              <UserProfilePage />
+            </Layout>
+          }
+        />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
 
-
-const AppRoutes = ()=>{
-    return (
-        <Routes>
-            <Route path="/"  element={<Layout showHero><HomePage /></Layout>}/>
-            <Route path="/auth-callback"  element={<AuthCallbackPage />}/>
-            <Route path="/user-profile" element={<Layout><UserProfilePage /></Layout>} />
-            <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-    )
-}
-
-
-export default AppRoutes
+export default AppRoutes;
