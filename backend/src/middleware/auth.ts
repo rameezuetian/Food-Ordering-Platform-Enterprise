a475d927513efcc5990bdbacaf4840c8d1e8143a
rameezuetian <muhammadrameez262@gmail.com>
2026-09-23 09:39:01 +0500

    updating the user

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,65 @@
+declare module "jsonwebtoken";
+
+import type { Request, Response, NextFunction } from "express";
+import { auth } from "express-oauth2-jwt-bearer";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+const audience = process.env.AUTH0_AUDIENCE;
+const issuerBaseURL = process.env.AUTH0_ISSUER_BASE_URL;
+
+if (!audience || !issuerBaseURL) {
+  throw new Error("Missing Auth0 environment variables");
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
+
+export const jwtCheck = auth({
+  audience,
+  issuerBaseURL,
+  tokenSigningAlg: "RS256",
+});
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.sendStatus(401);
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+
+    if (!decoded || !decoded.sub) {
+      return res.sendStatus(401);
+    }
+
+    const auth0Id = decoded.sub;
+
+    const user = await User.findOne({ auth0Id });
+
+    if (!user) {
+      return res.sendStatus(401);
+    }
+
+    req.auth0Id = auth0Id;
+    req.userId = user._id.toString();
+
+    next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
+};
--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import MyUserController from "../controllers/MyUserController.js";
+import jwtCheck from "../middleware/auth.js";
+
+
 
 const router = express.Router()
 
 
-router.post("/" , MyUserController.createCurrentUser);
+router.post("/" , jwtCheck ,MyUserController.createCurrentUser);
+router.put("/" , MyUserController.updateCurrentUser)
 
 
 
